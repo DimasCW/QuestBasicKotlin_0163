2fe6a35786e4b1671fcb7e8856f3e1aa8f21eb65
DimasCW <cdimas292@gmail.com>
2024-10-12 07:35:42 +0700

    menyelesaikan file colllection

--- app/src/main/java/com/example/myapplication/Collection.kt
+++ app/src/main/java/com/example/myapplication/Collection.kt
@@ -1,4 +1,4 @@
-fun Collection(){
+fun ContohList(){
     println("=== list ===")
     //List Read-Only
     val readOnlyAbjad = listOf("A", "B", "C")
@@ -53,4 +53,44 @@ fun ContohSet() {
     //Set read-only
     val shapesLocked: Set<String> = shape
     println(shapesLocked)
+}
+
+//Map adalah kumpulan data yang disusun dalam pasangan key-value
+//Map bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+//Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+//Map read only menggunakan MapOf
+//Map mutable menggunakan mutableMapOf
+
+fun ContohMap(){
+    println()
+    println("=== Map ===")
+    //Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    //Map Mutable
+    val shape:MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    //Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    //Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    //Mengubah data di dalam Map mutable
+    shape["Square"] = 5
+    println(shape)
+
+    //Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+
+    fun main(){
+        ContohList()
+        ContohSet()
+        ContohMap()
+    }
 }
