cba392a8a6b38b53ac3348e2e2d7f8f3f28640b5
DimasCW <cdimas292@gmail.com>
2024-10-12 08:36:49 +0700

    memperbaiki kesalahan

--- app/src/main/java/com/example/myapplication/Collection.kt
+++ app/src/main/java/com/example/myapplication/Collection.kt
@@ -61,32 +61,7 @@ fun ContohSet() {
 //Map read only menggunakan MapOf
 //Map mutable menggunakan mutableMapOf
 
-fun ContohMap(){
-    println()
-    println("=== Map ===")
-    //Map Read-Only
-    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
-    println(readOnlyShape)
 
-    //Map Mutable
-    val shape:MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
-    println(shape)
-
-    //Menambahkan data ke dalam Map Mutable
-    shape["Rectangle"] = 4
-    println(shape)
-
-    //Menghapus data dari Map Mutable
-    shape.remove("Circle")
-    println(shape)
-
-    //Mengubah data di dalam Map mutable
-    shape["Square"] = 5
-    println(shape)
-
-    //Map Read-Only
-    val shapesLocked: Map<String, Int> = shape
-    println(shapesLocked)
 
     fun main(){
         ContohList()
