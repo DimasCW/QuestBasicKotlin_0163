6a9287526ebb19e66dcccf36c9ce02e0f441a035
DimasCW <cdimas292@gmail.com>
2024-10-12 08:35:19 +0700

    menyelesaikan file NullSafety

--- app/src/main/java/com/example/myapplication/NullSafety.kt
+++ app/src/main/java/com/example/myapplication/NullSafety.kt
@@ -23,4 +23,14 @@ fun nullSafety(){
     println(neverNull.length) //18
     println(nullable?.length) //null
 
+    //notNull doesn't accept null values
+    fun strLength(notNull: String) : Int{
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) //18
+}
+
+fun main(){
+    nullSafety()
 }
